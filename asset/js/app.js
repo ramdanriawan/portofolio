b45c60e1c4214da31cf17bb569696b813fd745e6
ramdan3mts <ramdanriawan3@gmail.com>
2018-12-25 11:12:47 +0700

    gambar muncul
    gambarnya munculnya gantian

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $("#scroll-to-top").click(function(){
+$(document).ready(function () {
+    // untuk scroll to top
+    $("#scroll-to-top").click(function () {
         var target = $(this).attr('data-target');
 
         $('html').animate({
@@ -7,7 +8,8 @@ $(document).ready(function(){
         });
     });
 
-    $('.scroll-to-target').click(function(e){
+    // untuk scroll sesuai target
+    $('.scroll-to-target').click(function (e) {
         event.preventDefault();
 
         var href = $(this).attr('href');
@@ -16,6 +18,34 @@ $(document).ready(function(){
         $('html').animate({
             scrollTop: offsetHref - 50
         });
+    });
 
-    })
-})
+    // event window scroll untuk navbar dan thumbnail
+    $(window).scroll(function () {
+        var windowScrollPos = $(this).scrollTop();
+
+        // untuk navbar
+        var namaPos = $('#nama').offset().top;
+        var navbar = $('.navbar');
+
+        if (windowScrollPos >= namaPos) {
+            navbar.addClass('navbar-transparent');
+            navbar.removeClass('navbar-transparent-balik');
+        } else {
+            navbar.addClass('navbar-transparent-balik');
+            navbar.removeClass('navbar-transparent');
+        }
+
+        // untuk thumbnail
+        var thumbnail = $('.img-portofolio .thumbnail');
+        var imgPortofolioPos = $('.img-portofolio').offset().top;
+
+        if (windowScrollPos >= imgPortofolioPos - 500) {
+            $.each(thumbnail, function (index) {
+                setTimeout(function () {
+                    thumbnail.eq(index).addClass('muncul');
+                }, 300 * index);
+            });
+        }
+    });
+});
